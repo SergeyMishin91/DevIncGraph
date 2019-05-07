2a97d3d70f73bee9440e3f5c1c7aa00f19a47e96
Sergey Mishin <smishin91@yandex.ru>
2019-05-08 01:52:54 +0300

    add Data

--- DevIncGraph/Controllers/HomeController.cs
+++ DevIncGraph/Controllers/HomeController.cs
@@ -2,22 +2,30 @@
 using Microsoft.AspNetCore.Mvc;
 using DevIncGraph.Models;
 using DevIncGraph.Models.ViewModels;
+using DevIncGraph.Data;
 
 namespace DevIncGraph.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DevIncGraphContext _context;
+
+        public HomeController(DevIncGraphContext context)
+        {
+            _context = context;
+        }
+
         UserData userData = new UserData();
 
         public IActionResult Index()
         {
-            //Point point = new Point();
-            //point.PointX = 1;
-            //point.PointY = 1;
-            //userData.Points.Add(point);
-            //IndexViewModel viewModel = new IndexViewModel();
-            //viewModel.Points = userData.Points;
-            return View(/*viewModel*/);
+            Point point = new Point();
+            point.PointX = 1;
+            point.PointY = 1;
+            userData.Points.Add(point);
+            IndexViewModel viewModel = new IndexViewModel();
+            viewModel.Points = userData.Points;
+            return View(viewModel);
         }
 
         [HttpPost]
@@ -37,11 +45,15 @@ namespace DevIncGraph.Controllers
                 while (userData.RangeFrom <= userData.RangeTo)
                 {
                     Point point = new Point();
+                    
                     point.PointX = userData.RangeFrom;
                     point.PointY = userData.A * (int)Math.Pow(point.PointX, 2) + userData.B * point.PointX + userData.C;
                     userData.RangeFrom += (int)userData.Step;
                     userData.Points.Add(point);
+                    _context.ContextPoints.Add(point);
                 }
+                _context.ContextUserDatas.Add(userData);
+                _context.SaveChanges();
                 viewModel.Points = userData.Points;
                 viewModel.UserData = userData;
                 return View(viewModel);
--- DevIncGraph/Models/Point.cs
+++ DevIncGraph/Models/Point.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace DevIncGraph.Models
+﻿namespace DevIncGraph.Models
 {
     public class Point
     {
